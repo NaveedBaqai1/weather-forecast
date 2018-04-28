64bdc4d7584e78a596cc911d0451008ceca96bdd
Naveed Baqai <naveed@hibirdi.com>
2018-04-28 10:25:33 +0100

    import bootstrap, font awsome, axios. set empty store for state management and render some bootstrap empty elements

--- darkSky/src/components/Home.vue
+++ darkSky/src/components/Home.vue
@@ -1,11 +1,34 @@
 <template>
-  <div>
-   Hello World!
-  </div>
+    <main role="main">
+      <section class="jumbotron">
+        <div class="container">
+
+        </div>
+      </section>
+      <div class="album py-5 bg-light">
+        <div class="container">
+
+        </div>
+      </div>
+    </main>
 </template>
 
 <script>
   export default {
-    name: 'Home'
+    name: 'Home',
+    data () {
+      return {
+        store: this.$store
+      }
+    },
+    mounted () {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    }
   }
 </script>
